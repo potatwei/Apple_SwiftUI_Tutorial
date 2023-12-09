0b5706761159aa1ff97e44c75180caea802253ea
Shihang Wei <wei13904639403@gmail.com>
2023-12-09 13:37:52 -0500

    hiking graph created

---
+++ Landmarks/Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,29 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Shihang Wei on 12/9/23.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+//        Path { path in
+//            var width: CGFloat = 100.0
+//            let height = width
+//            path.move(
+//                to: CGPoint(
+//                    x: width * 0.95,
+//                    y: height * 0.2
+//                )
+//            )
+//        }
+//        .fill(.black)
+        Text("place holder")
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
