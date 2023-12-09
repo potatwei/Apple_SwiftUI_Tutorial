1c185b125ab86dc00751df1da3d18165ad55ffc1
Shihang Wei <wei13904639403@gmail.com>
2023-12-08 23:03:06 -0500

    LandMark app created

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Shihang Wei on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
