850625f04c8a6d75d3b72300a6e8814fa3deee2d
Shihang Wei <wei13904639403@gmail.com>
2023-12-09 01:05:08 -0500

    landscape refined

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Shihang Wei on 12/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Shihang Wei on 12/8/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview("Turtle Rock") {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
+}
+
+//#Preview("Salmon") {
+//    LandmarkRow(landmark: landmarks[1])
+//}
